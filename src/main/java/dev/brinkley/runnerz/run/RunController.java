941f12026f248672f8e449effd13f4a920536c5b
tafadzwamadzanire <tafadzwa.madzanire@fbc.co.zw>
2025-02-04 10:35:18 +0200

    Adding validation to input record data

--- src/main/java/dev/brinkley/runnerz/run/RunController.java
+++ src/main/java/dev/brinkley/runnerz/run/RunController.java
@@ -26,7 +26,7 @@ public class RunController {
     Run findById(@PathVariable Integer id ){
       Optional<Run> run =  runRepository.findById(id);
       if(run.isEmpty()){
-          throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Run not found");
+          throw new RunNotFoundException(id);
       }
 
       return run.get();
